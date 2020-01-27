1358d10b96cd30cd52a9e81aed1ffafbdad6fa1b
Anna Dybas <a.m.dybas@gmail.com>
2020-01-27 11:35:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Your code goes here
-document.addEventListener("DOMContentLoaded", function(){
+// document.addEventListener("DOMContentLoaded", someText);
+
+// function someText() {
+//     document.getElementById("text").textContent = "This is really cool!"
+// }
+
+document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("text").textContent = "This is really cool!"
 });
