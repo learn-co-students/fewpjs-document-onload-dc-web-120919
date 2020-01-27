05e672461669fd68bf074c99b7151f67b5cda081
github username <github email address>
2020-01-27 14:35:14 -0500

    pause

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ document.addEventListener( "DOMContentLoaded", function () {
     updateDOM()
   } );
 
-function updateDom () {
-    document.getElementById( "text ").innerHTML = "This is really cool";
+function updateDom() {
+    document.getElementById( "text ")
+    .innerHTML = "This is really cool";
 }
+
